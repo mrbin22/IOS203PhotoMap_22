aa461f7a0f10c1782ed5151d9dd23941f6989015
mrbin22 <23cm0122@jec.ac.jp>
2024-05-21 13:45:24 +0900

    Initial Commit

---
+++ IOS203PhotoMap_22/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS203PhotoMap_22
+//
+//  Created by cmStudent on 2024/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS203PhotoMap_22/IOS203PhotoMap_22App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS203PhotoMap_22App.swift
+//  IOS203PhotoMap_22
+//
+//  Created by cmStudent on 2024/05/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS203PhotoMap_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
